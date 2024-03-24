06ef997b30db7d9d75a6a255b67cb707f6a0ee64
Wojciech Paluch <wojciech.paluch1993@gmail.com>
2024-03-25 00:58:33 +0100

    Add lazygit in fterm window

---
+++ lua/custom/plugins/fterm.lua
@@ -0,0 +1,19 @@
+return {
+  'numtostr/FTerm.nvim',
+  config = function()
+    local fterm = require 'FTerm'
+    local map = vim.keymap.set
+
+    fterm.setup {
+      cmd = 'lazygit',
+      dimensions = {
+        height = 0.9,
+        width = 0.95,
+      },
+      ft = 'lazygit',
+    }
+
+    map('n', '<C-g>', fterm.toggle)
+    map('t', '<C-g>', "<C-\\><C-n><cmd>lua require('FTerm').toggle()<CR>")
+  end,
+}
